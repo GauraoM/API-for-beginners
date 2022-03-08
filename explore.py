79e1ef19b1dc1303e4e42802524309ce5128bd6a
gaurao <gaurao.mate@konverge.ai>
2022-03-08 09:50:01 +0530

    Using helper libraries python

--- explore.py
+++ explore.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 client = Client("ACcc36e92e48f99e1dd6d6330fe40c244f","0d1f3c27f724c7ee63e0acc1d0eed0c0")
 
 # Iterate through messages and see all message
-#for message in client.messages.list():
-#    print(message.body)
+for message in client.messages.list():
+    print(message.body)
 
 # Deleting the message
 for message in client.message.list():
@@ -13,10 +13,10 @@ for message in client.message.list():
     message.delete()
 
 # Send new message
-# msg = client.messages.create(
-#     to = "+919860518381",
-#     from_ = "+19036231488",
-#     body= "Hello from Python..."
-# )    
+msg = client.messages.create(
+    to = "+919860518381",
+    from_ = "+19036231488",
+    body= "Hello from Python..."
+)    
 
-# print(f"new message created: {msg.sid}")
+print(f"new message created: {msg.sid}")
